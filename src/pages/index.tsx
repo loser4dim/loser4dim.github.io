8494d1aa942967e1e36ae4bf276ea97914599898
loser4dim <loser4dim@gmail.com>
2023-08-23 19:15:04 +0900

    Modify Size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Head  from "next/head";
 import Image from "next/image";
 import Link  from "next/link";
 
-import {
+/*import {
   useEffect
-} from "react";
+} from "react";*/
 
 import {
   FontAwesomeIcon
@@ -23,31 +23,34 @@ import {
   faXTwitter
 } from "@fortawesome/free-brands-svg-icons";
 
+/*const handleResize = () => {
+  if (!window) {
+    return;
+  }
+  
+  const LOGO_IMAGE_RATIO = 1247.0 / 1835.0;
+
+  const logo_img = document.getElementById("logo_image") as HTMLElement;
+
+  const new_width  = (window.innerWidth / window.innerHeight < LOGO_IMAGE_RATIO) ? window.innerWidth                    : window.innerHeight * LOGO_IMAGE_RATIO;
+  const new_height = (window.innerWidth / window.innerHeight < LOGO_IMAGE_RATIO) ? window.innerWidth / LOGO_IMAGE_RATIO : window.innerHeight;
+  //logo_img.setAttribute("width" , (new_width).toString());
+  //logo_img.setAttribute("height", (new_height).toString());
+
+  return;
+}*/
+
 const IndexPage = () => {
-  useEffect(
+  /*useEffect(
     () => {
       if (!window) {
         return;
       }
-
-      const handleResize = () => {
-        const LOGO_IMAGE_RATIO = 1247.0 / 1835.0;
-
-        const logo_img = document.getElementById("logo_image") as HTMLElement;
-
-        const new_width  = (window.innerWidth / window.innerHeight < LOGO_IMAGE_RATIO) ? window.innerWidth                    : window.innerHeight * LOGO_IMAGE_RATIO;
-        const new_height = (window.innerWidth / window.innerHeight < LOGO_IMAGE_RATIO) ? window.innerWidth / LOGO_IMAGE_RATIO : window.innerHeight;
-        logo_img.setAttribute("width" , (new_width).toString());
-        logo_img.setAttribute("height", (new_height).toString());
-
-        return;
-      }
-      handleResize();
       window.addEventListener("resize", handleResize);
       return;
     },
     []
-  );
+  );*/
 
   return(
     <div className = {styles["IndexPage"]}>
@@ -78,99 +81,116 @@ const IndexPage = () => {
         </noscript>
         <div className = {styles["IndexLogo"]}>
           <Image
-            id     = "logo_image"
-            src    = "/Resources/logo.webp"
-            width  = {1247}
-            height = {1835}
-            alt    = "logo"
+            id      = "logo_image"
+            src     = "/Resources/logo.webp"
+            fill    = {true}
+            alt     = "logo"
             quality = {100}
+            sizes   = "(max-width: 1247px) 100vw"
+            style   = {
+              {
+                objectFit: "contain"
+              }
+            }
           />
         </div>
-        <div className = {styles["IndexLinks"]}>
+        <div className = {styles["IndexLists"]}>
           <nav>
-              <ul className = {styles["IndexLinksList"]}>
-                <li className = {styles["IndexLinksListContent"]}>
+              <ul className = {styles["IndexListsColumn"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://www.mixcloud.com/loser4dim/">
                     <FontAwesomeIcon icon={faMixcloud} />
                   </a>
                 </li>
 
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://soundcloud.com/loser4dim/tracks">
                     <FontAwesomeIcon icon={faSoundcloud} />
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://bandcamp.com/loser4dim/">
                     <FontAwesomeIcon icon={faBandcamp} />
                   </a>
                 </li>
-              </ul>
-              <ul  className = {styles["IndexLinksList"]}>
-                <li className = {styles["IndexLinksListContent"]}>
+
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://instagram.com/loser4dim/">
                     <FontAwesomeIcon icon={faInstagram}/>
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://www.threads.net/@loser4dim/">
                     <FontAwesomeIcon icon={faThreads} />
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://x.com/loser4dim/">
                     <FontAwesomeIcon icon={faXTwitter} />
                   </a>
                 </li>
-              </ul>
-              <ul  className = {styles["IndexLinksList"]}>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://github.com/loser4dim/">
                     <FontAwesomeIcon icon={faGithub}/>
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://www.twitch.tv/loser4dim/">
                     <FontAwesomeIcon icon={faTwitch} />
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://discord.gg/3mPudy837n">
                     <FontAwesomeIcon icon={faDiscord} />
                   </a>
                 </li>
-              </ul>
-              <ul  className = {styles["IndexLinksList"]}>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://misskey.io/@loser4dim/">
+                    <p>
                     <Image
                       id     = "io_img"
                       src    = "/Resources/MisskeyIO.webp"
-                      width  = {64}
-                      height = {64}
-                      alt    = "logo"
+                      fill   = {true}
+                      alt    = "MisskeyIO"
+                      sizes  = "(max-height: 50pt) 8vmin"
+                      style  = {
+                        {
+                          objectFit: "contain"
+                        }
+                      }
                     />
+                    </p>
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://misskey.niri.la/@loser4dim/">
                     <Image
                       id     = "nirila_img"
                       src    = "/Resources/MisskeyNirila.png"
-                      width  = {64}
-                      height = {64}
-                      alt    = "logo"
+                      fill   = {true}
+                      alt    = "MisskeyNirila"
+                      sizes  = "(max-height: 50pt) 8vmin"
+                      style  = {
+                        {
+                          objectFit: "contain"
+                        }
+                      }
                     />
                   </a>
                 </li>
-                <li className = {styles["IndexLinksListContent"]}>
+                <li className = {styles["IndexListsColumnContent"]}>
                   <a href="https://vrchat.com/home/user/usr_22d7c048-3c45-478b-b492-88347fbbc1fc">
                     <Image
                       id     = "vrchat_img"
-                      src    = "/Resources/VRChat.svg"
-                      width  = {128}
-                      height = {64}
+                      src    = "/Resources/VRChat.png"
+                      fill   = {true}
                       alt    = "logo"
+                      sizes  = "(max-height: 50pt) 8vmin"
+                      style  = {
+                        {
+                          objectFit: "contain"
+                        }
+                      }
                     />
                   </a>
                 </li>
